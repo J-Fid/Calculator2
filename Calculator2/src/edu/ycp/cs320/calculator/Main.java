ca101b2b496848f4ff6d8a092934b0af3d147f6d
Jason Fiddle <jfiddle@ycp.edu>
2013-02-06 11:38:09 -0500

    Added guessing game.

--- Calculator2/src/edu/ycp/cs320/calculator/Main.java
+++ Calculator2/src/edu/ycp/cs320/calculator/Main.java
@@ -1,9 +1,27 @@
 package edu.ycp.cs320.calculator;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the calculator");
 		System.out.println("Fasten your seatbelt!");
 		
+		int i, sum, guess;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter an Integer please.");
+		i = scan.nextInt();
+		
+		sum = i * 47;
+		
+		System.out.println("You entered " + i + ", and the result was " + sum + ". What is the other number?");
+		
+		guess = scan.nextInt();
+		
+		if(guess == 47)
+			System.out.println("Correct!");
+		else
+			System.out.println("Nope!");
 	}
 }
